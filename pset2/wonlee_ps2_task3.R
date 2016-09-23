8bd38f9ee7ad179874df584398160da9525ae91e
W Ryan Lee <ryanwonlee@gmail.com>
2016-09-23 14:39:58 -0400

    pset2

--- pset2/wonlee_ps2_task3.R
+++ pset2/wonlee_ps2_task3.R
@@ -17,32 +17,45 @@ sigmas = c(0.7, 1.3, 1.3, 1.6)
 J = 1000
 N = 2
 w = rep(1, J)
-ndat = 25 # Number of data sets per simulation (1 theta per simulation)
+nsims = 11 # Number of theta draws per simulation
+ndat = 25 # Number of data sets per theta
 ndraws = 1e4
-
-# Simulate data
 curr_sim = strtoi(args[1])
 mu = mus[curr_sim]
 sigma = sigmas[curr_sim]
-log.theta = rnorm(J, mu, sigma)
 
-# Run MCMC to obtain posterior
-post.means = matrix(rep(0, ndat*J), nrow = J)
-post.std = matrix(rep(0, ndat*J), nrow = J)
-lb.95 = matrix(rep(0, ndat*J), nrow = J)
-ub.95 = matrix(rep(0, ndat*J), nrow = J)
-start = proc.time()
-for (i in 1:ndat) {
-  print(i)
-  Y = matrix(c(rpois(J, exp(log.theta)), rpois(J, exp(log.theta))), ncol = N)
-  results = poisson.logn.mcmc(Y, w)$logTheta
-  post.means[,i] = rowMeans(results)
-  post.std[,i] = rowSds(results)
-  lb.95[,i] = apply(results, 1, function(x) return(sort(x)[floor(0.025*ndraws)]))
-  ub.95[,i] = apply(results, 1, function(x) return(sort(x)[floor(0.975*ndraws)]))
+# Store simulation values
+log.thetas = matrix(rep(0, J*nsims), nrow = J)
+coverage.probs = matrix(rep(0, J*nsims), nrow = J)
+runtimes = rep(0, nsims)
+
+for (t = 1:nsims) {
+  print(t)
+  # Simulate theta
+  log.thetas[,t] = rnorm(J, mu, sigma)
+  # Run MCMC to obtain posterior
+  lb.95 = matrix(rep(0, ndat*J), nrow = J)
+  ub.95 = matrix(rep(0, ndat*J), nrow = J)
+  cov = rep(0, J)
+  start = proc.time()
+  for (i in 1:ndat) {
+    print(i)
+    Y = matrix(c(rpois(J, exp(log.theta)), rpois(J, exp(log.theta))), ncol = N)
+    results = poisson.logn.mcmc(Y, w)$logTheta
+    post.means[,i] = rowMeans(results)
+    post.std[,i] = rowSds(results)
+    lb.95[,i] = apply(results, 1, function(x) return(sort(x)[floor(0.025*ndraws)]))
+    ub.95[,i] = apply(results, 1, function(x) return(sort(x)[floor(0.975*ndraws)]))
+    for (j in 1:J) {
+      if (log.thetas[j,t] > lb.95[j,i]) & (log.thetas[j,t] < ub.95[j,i]) {
+        cov[j] = cov[j] + 1
+      }
+    }
+  }
+  coverage.probs[,t] = cov/ndat
+  runtimes[t] = (proc.time() - start)[3]
 }
-time = (proc.time() - start)[3]
-print(time)
 
+# Plot estimate coverage against log theta
 
 
